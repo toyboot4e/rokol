8e74454689ab27c27b9cb95626ab68a57067c1d8
toyboot4e <toyboot4e@gmail.com>
2020-11-17 17:32:32 +0900

    fix glsl quad shader

--- examples/shaders/glsl/quad_fs.glsl
+++ examples/shaders/glsl/quad_fs.glsl
@@ -5,3 +5,4 @@ out vec4 frag_color;
 void main() {
     frag_color = color;
 }
+
--- examples/shaders/glsl/quad_vs.glsl
+++ examples/shaders/glsl/quad_vs.glsl
@@ -1,9 +1,11 @@
 #version 330
-in vec4 position;
-in vec4 color0;
+layout(location=0) in vec4 position;
+layout(location=1) in vec4 color0;
+
 out vec4 color;
 
 void main() {
     gl_Position = position;
     color = color0;
 }
+

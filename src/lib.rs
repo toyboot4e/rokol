e8a4d4a9341899930e61a6947302158e526f0b62
toyboot4e <toyboot4e@gmail.com>
2020-11-20 02:56:30 +0900

    rokol is so different from sokol-rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
 //! Rokol, wrapper of Sokol libraries
-//!
-//! Rokol is based on [sokol-rs]. Many thanks to them!
-//!
-//! [sokol-rs]: https://github.com/code-disaster/sokol-rs
 
 pub use rokol_ffi as ffi;
 use std::ffi::CString;

b48a903b832155ef52a5dd1496fcc00846fa017d
toyboot4e <toyboot4e@gmail.com>
2020-12-07 20:43:10 +0900

    add missimg imgui wrappers

---
+++ rokol_ffi/wrappers/imgui.h
@@ -0,0 +1,12 @@
+//! File for generating Rust FFI
+
+#define SOKOL_TRACE_HOOKS
+
+// blacklisted includes
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+
+// simgui*
+#include "sokol_imgui.h"
+// sg_imgui*
+#include "sokol_gfx_imgui.h"
